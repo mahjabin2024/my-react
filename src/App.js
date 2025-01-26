688f496df3f9a622be9acbf62261d6d88c03c520
mahjabin2024 <mahjabin.mahjabin@opetus.tampere.fi>
2025-01-26 13:20:53 +0200

    react2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './components/Welcome';
+import Goal from './components/Goal';
 
 function App() {
   return (
     <div className="App">
-     <Welcome></Welcome>
+    <Goal></Goal>
     </div>
   );
 }
---
+++ src/components/Goal.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+function Goal(){
+return(
+<h2>My goal is to become an Engineer.</h2>
+)
+}
+
+export default Goal; 
